05f676006ba8c1a9696ef640d3c7d1ea8ccf0055
davidfurlong <dvfurlong@googlemail.com>
2014-12-15 15:11:19 +0100

    new nav, changes to homepage

--- config/beta-request-database.js
+++ config/beta-request-database.js
@@ -1,5 +1,6 @@
 url = process.env.MONGOHQ_URL ||
-	'mongodb://localhost:27017';
+	'mongodb://wzeng:mongotester@kahana.mongohq.com:10078/app27380840';
+	// 'mongodb://localhost:27017';
   // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
 
 module.exports = {
