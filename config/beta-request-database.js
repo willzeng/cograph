f36d27d88a0d8a5756f4135ce1d9a364a84531d9
Will Zeng <willzeng@users.noreply.github.com>
2015-01-31 15:04:22 +0000

    Update beta-request-database.js

--- config/beta-request-database.js
+++ config/beta-request-database.js
@@ -1,4 +1,6 @@
-url = process.env.MONGOHQ_URL;
+url = -url = process.env.MONGOHQ_URL ||
+-	'mongodb://wzeng:mongotester@kahana.mongohq.com:10078/app27380840';
+-	// 'mongodb://localhost:27017';
 
 module.exports = {
   'url' :  url
