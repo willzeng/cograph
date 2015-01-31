c5fccace7f55467b75de7712e42379599e976c50
Will Zeng <willzeng@users.noreply.github.com>
2015-01-31 15:01:56 +0000

    updb file with comment

--- config/database.js
+++ config/database.js
@@ -1,8 +1,7 @@
 // config/database.js
 url = process.env.MONGOHQ_URL ||
 	// 'mongodb://localhost:27017';
-  'mongodb://wzeng:mongotester@kahana.mongohq.com:10078/app27380840';
-  // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+  'mongodb://wzeng:mongotester@kahana.mongohq.com:10078/app27380840'; //This is just a public sandbox database of users for local testing
 
 module.exports = {
   'url' :  url
