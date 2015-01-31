3f01858e28191be6c2002da53748584ee5da0ac6
willzeng <zeng.will@gmail.com>
2015-01-31 15:06:12 +0000

    typo

--- config/beta-request-database.js
+++ config/beta-request-database.js
@@ -1,4 +1,4 @@
-url = -url = process.env.MONGOHQ_URL || 'mongodb://wzeng:mongotester@kahana.mongohq.com:10078/app27380840';
+url = process.env.MONGOHQ_URL || 'mongodb://wzeng:mongotester@kahana.mongohq.com:10078/app27380840';
 
 module.exports = {
   'url' :  url
